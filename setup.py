8394ce8d409af515b11a9ef9b9c4e4f79885b19e
Jeremy Nelson <jermnelson.gmail.com>
2013-12-10 14:41:08 -0700

    Added more dependencies and updated version

--- setup.py
+++ setup.py
@@ -2,29 +2,34 @@
 Flask-FedoraCommons
 -------------------
 
-This Flask extension read/write access to the digital repository `Fedora
-Commons <http://fedora-commons.org/>`_ for use by Flask applications
+This Flask extension provides CRUD operations for
+`Fedora Commons <http://fedora-commons.org/>`_ digital repositories.
 
 """
 __author__ = "Jeremy Nelson"
 
-from setuptools import setup
+from setuptools import find_packages, setup
+
+
 
 setup(
     name='Flask-FedoraCommons',
-    version='0.0.4',
+    version='0.0.5',
     url='http://github.com/jermnelson/flask-fedora-commons',
     license='MIT License',
     author=__author__,
     author_email='jermnelson@gmail.com',
     description='Library for manipulating Fedora Commons digitial repositories',
     long_description=__doc__,
-    py_modules=['flask_fedora_commons'],
+    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     install_requires=[
-        'Flask'
+        'python-dateutil',
+        'Flask',
+        'poster',
+        'rdflib'
     ],
     classifiers=[
         'Framework :: Flask',
