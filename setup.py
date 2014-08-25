b086918b0079356c5043d4fcc870ffc51551043d
Jeremy Nelson <jermnelson.gmail.com>
2014-08-25 16:44:44 -0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ Flask-FedoraCommons
 -------------------
 
 This Flask extension provides CRUD operations for
-`Fedora Commons <http://fedora-commons.org/>`_ digital repositories.
+`Fedora Commons <http://fedora-commons.org/>`_ digital repositories. Latest
+version focuses on `Fedora 4 <https://wiki.duraspace.org/display/FF/Fedora+Repository+Home>`_
+using Python 3.
 
+Legacy support for Fedora 3.x and Python 2.7x are available by cloning the project's
+git repository at <https://github.com/jermnelson/flask-fedora-commons.git> and
+then checking out the legacy branch.
 """
 __author__ = "Jeremy Nelson"
 __version_info__ = ('0', '0', '8')
 __version__ = '.'.join(__version_info__)
 
 from setuptools import find_packages, setup
-
-
 
 setup(
     name='Flask-FedoraCommons',
@@ -39,6 +42,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
 
